d9b5f094b675e84426efd63d3a66e2a11d14714c
Kate <ekaterina.muradyan7@gmail.com>
2021-08-30 04:22:32 +0400

    deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarMenuComponent } from './components/navbar-menu/navbar-menu.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarMenuComponent,
+    HomeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
